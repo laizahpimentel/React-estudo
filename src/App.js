b4285048b41e2b639e53a96de0cf8134cdfbd5db
laizahpimentel <laizahpimentel@gmail.com>
2023-04-29 03:02:23 -0300

    add components e props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 //componente de uma linha só, por isso comp direto
 // const Testcompont = () => <h2> testando componente </h2>
@@ -14,24 +15,53 @@ import React from 'react';
 // }
 
 //Componente com props
-const Testcompont = (props) => {
+// const Bemvindo = (props) => {
+//   return(
+//     <div>
+//       <h2>Bem vindo(a) {props.name} </h2>
+//       <h3> tenho {props.age} anos!</h3>
+//     </div>
+//   );
+// }
+
+const Equipe = (props)  => {
+  // return <a> Funcionou </a>
   return(
     <div>
-      <h2>Bem vindo(a) {props.name} </h2>
-      <h3> tenho {props.age} anos!</h3>
+      <Sobre username={props.name}  job={props.job} age={props.age} />
+      <Social inst={props.instagram}  fb={props.facebook} />
+      <hr />
     </div>
   );
+}
+
+const Sobre = (props) =>{
+  return(
+    <div>
+      <h2> Olá sou o(a)  {props.username} </h2>
+      <h2>cargo: {props.job} </h2>
+      <h2> minha idade é:  {props.age} </h2>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return(
+    <div>
+      <a href={props.inst}>Instagram</a>
+        <a href={props.fb}>Facebook</a>
+        <a>LinkedIn</a>
+    </div>
+  )
 }
 
 
 function App(){
   return(
     <div>
-      Olá mundo!
-      <Testcompont  name="Laíza" age="24"/>
-      <Testcompont  name="Kevin" age="24"/>
-      <Testcompont  name="Nho" age="5"/>
-      <h1>Aprendendo componentes e props</h1>
+      <h1> venha conhecer nosso time </h1>
+      <Equipe name="Iza" job="dev" age="24" instagram="https://instagram.com" />
+      <Equipe name="Kevin" job="tecnico" age="24"  facebook="https://facebook.com" />
     </div>
   )
 }
